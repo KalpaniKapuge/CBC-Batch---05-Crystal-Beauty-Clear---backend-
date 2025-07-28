1ae63fc0d196e54e46b7834e629b87ea7c15497e
KalpaniKapuge <kalpanikapuge1020@gmail.com>
2025-07-29 01:25:56 +0530

    Implement product deletion✏️

--- controllers/productController.js
+++ controllers/productController.js
@@ -24,3 +24,41 @@ export function saveProduct(req, res) {
             });
         });
 }
+
+export async function getProducts(req,res){
+    try{
+        if(isAdmin(req)){
+            const products = await Product.find();
+            res.status(200).json(products);
+        }else{
+            const products = await Product.find({isAvailable: true});
+            res.status(200).json(products);
+        }
+    }catch(err){
+        res.status(500).json({
+            message: "Failed to get products",
+            error: err
+        });
+    }
+}
+
+export async function deleteProduct(req, res) {
+    if (!isAdmin(req)) {
+        res.status(403).json({
+            message: "You are not authorized to delete a product"
+        })
+        return
+    }
+    try{
+        await Product.deleteOne({productId: req.params.productId});
+        res.status(200).json({
+            message: "Product deleted successfully"
+        });
+    }catch(err){
+        res.status(500).json({
+            message: "Failed to delete product",
+            error: err
+        });
+    }
+    
+}
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { saveProduct } from '../controllers/productController.js';
+import { saveProduct , deleteProduct, getProducts} from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post("/",saveProduct);
+productRouter.delete("/:productId", deleteProduct);
+productRouter.get("/", getProducts);
 
 export default productRouter;
